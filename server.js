61551f98b4aba7009579534f01c9d111e11cbea7
Marlon vargas <20240088@itla.edu.do>
2026-03-31 22:07:35 -0400

    feat: setup inicial arquitectura limpia

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const express = require('express');
+const { dbConnection } = require('./src/infrastructure/database/config');
+const app = express();
+dbConnection();
+app.use(express.json());
+app.get('/', (req, res) => res.send('SUPERMERCADO-API-READY'));
+app.listen(process.env.PORT, () => console.log('Server running'));
